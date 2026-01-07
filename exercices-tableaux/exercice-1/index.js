dff3e9a8b9c70017912e079da934b6232231f89c
Mariyam Abdul Rauf <marium205@hotmail.com>
2026-01-07 09:37:14 +0000

    Exercice 1 fini

--- exercices-tableaux/exercice-1/index.js
+++ exercices-tableaux/exercice-1/index.js
@@ -1,5 +1,5 @@
 let animals = ["Dog", "Cat", "Spider", "Bat", "Snake", "Bear"];  //Afficher tous les éléments du tableau animals avec la syntaxe simple
-for (let i = 0; i < animals.length; i++) {
+for (let i = 0; i < animals.length; i++) { //for cest un boucle dcp sa se repete plusieres fois "let = i" i est un compteur et on commence a 0 parceque sa commence toujours a 0"animals length"= cest combiens de animals ya et la yen a 6 dcp tant que i est inferieur que 6 le boucle continue (=i + 1  le boucle peut pas etre en infinie cest a eviter)
     console.log(animals[i]);
 }
 
@@ -7,3 +7,4 @@ let gods = ["Zeus", "Persephone", "Dyonisos", "Artemis", "Ares"];  // Afficher t
 for (let god of gods) {
     console.log(god);
 }
+//for..of la on veut just les valeurs c'est pour sa cest plus simple le tableaux parceque java fais pour moi 
