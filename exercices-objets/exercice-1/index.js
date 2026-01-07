1286f3fe2c2454fba14997a887108887e83efce1
Mariyam Abdul Rauf <marium205@hotmail.com>
2026-01-07 13:36:11 +0000

    Exercice 1 : créer un objet﻿

---
+++ exercices-objets/exercice-1/index.js
@@ -0,0 +1,9 @@
+const student = {
+	firstName : "Marium",
+	lastName : "Abdul Rauf",
+	age : 18,
+	average : 17
+};
+console.log(
+    `Nom : ${student.lastName}, Prenom : ${student.firstName}, Age : ${student.age}, Moyenne : ${student.average}`
+    );
