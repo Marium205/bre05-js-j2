40d402059fc0c6590d404d830dccfc3846654744
Mariyam Abdul Rauf <marium205@hotmail.com>
2026-01-07 10:19:17 +0000

    Exercice 3 fini with explications

--- exercices-tableaux/exercice-3/index.js
+++ exercices-tableaux/exercice-3/index.js
@@ -1,7 +1,7 @@
-let numbers = [10, 11, 15, 6];
+let numbers = [10, 11, 15, 6]; //la liste des nombres
 
-let sum = 0;
-for (let number of numbers) {
-    sum = sum + number;
+let sum = 0; // faut rajouter 0 sinon on peut pas additioner
+for (let number of numbers) { // la c'est la boucle for of comme ta fais avant RAPPEL pour chaque nombre dans le tableau numbers (number 10 11 15 6)
+    sum = sum + number; //la tu fais additions dcp sum + number 0 + 10 , 10+ 11 etc..
 }
-console.log(sum);
+console.log(sum); // et la sa s'affiche le sum total 42 dans le console
